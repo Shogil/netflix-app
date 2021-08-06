bd2c2c526a4bea6dfb36aee64d3eb6a7db55a091
shogil <shogilbabu@gmail.com>
2021-08-06 20:59:06 +0530

    update the content

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from "./Components/NavBar/NavBar"
 import './App.css';
+import Banner from './Components/Banner/Banner';
 
 function App() {
   return (
     <div className="App">
        <Navbar/>
+       <Banner/>
     </div>
   );
 }
---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Banner.css'
+
+function Banner() {
+    return (
+        <div className='banner'>
+            <div className='content'>
+                <h1 className='title'>Movie Name</h1>
+                <div className='banner_buttons'>\
+                   <button className='button'>Play</button>
+                   <button className='button'>Movie list</button>
+
+                </div>
+                <h1 className='description'>In publishing and graphic design</h1>
+
+            </div>
+            
+        </div>
+    )
+}
+
+export default Banner
